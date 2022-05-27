e3ef999742f9ab7a4020c05c298c3166ed443546
Vera957 <good.answer.e@gmail.com>
2022-05-28 01:34:29 +0300

    +

--- js/task-08.js
+++ js/task-08.js
@@ -10,10 +10,11 @@ function onFormSubmit(event) {
   event.preventDefault();
   if (formEl.email.value === "" || formEl.password.value === "") {
     alert("all fields must be filled in");
-  }
+  } else {
     const formValues = new FormData(event.currentTarget);
     formValues.forEach((value, name) => {
-        console.log(name + ":", value)
+      console.log(name + ":", value);
     });
     formEl.reset();
+  }
 }
